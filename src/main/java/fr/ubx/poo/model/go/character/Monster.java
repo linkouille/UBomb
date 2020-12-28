bc233fe93e69f8afaddcf915845b7ae0383d07e7
linkouille <linkouillou@gmail.com>
2020-12-28 15:53:41 +0100

    Basic Monster IA

--- src/main/java/fr/ubx/poo/model/go/character/Monster.java
+++ src/main/java/fr/ubx/poo/model/go/character/Monster.java
@@ -1,36 +1,65 @@
 package fr.ubx.poo.model.go.character;
 
+import fr.ubx.poo.engine.Timer;
 import fr.ubx.poo.game.Direction;
 import fr.ubx.poo.game.Game;
 import fr.ubx.poo.game.Position;
 import fr.ubx.poo.model.Movable;
+import fr.ubx.poo.model.decor.Decor;
 import fr.ubx.poo.model.go.GameObject;
+import fr.ubx.poo.model.go.collectable.Collectable;
 
 public class Monster extends Character implements Movable {
 
-    Direction direction;
+    private Direction direction;
+
+    private Timer movetimer;
 
     public Direction getDirection() {
         return direction;
     }
 
-    public Monster(Game game, Position position) {
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Monster(Game game, Position position, float speed) {
         super(game, position);
         this.direction = Direction.W;
         setCanTakeDamage(true);
         setAlive(true);
         setLives(1);
+        movetimer = new Timer(1);
     }
 
     @Override
     public boolean canMove(Direction direction) {
-        return false;
+        Position p = direction.nextPosition(this.getPosition());
+        if(!game.getWorld().isInside(p))
+            return false;
+
+        Decor d = game.getWorld().get(p);
+        if(d != null){
+            return d.canWalkOn();
+        }
+
+        // In the next position we have a GameObject
+        GameObject g = game.getWorld().getGO(p);
+        if(g != null){
+            return g.canWalkOn();
+        }
+
+        return true;
     }
 
     @Override
     public void doMove(Direction direction) {
+        Position nextPos = direction.nextPosition(getPosition());
+        setPosition(nextPos);
 
+        setDirection(direction);
     }
+
     public boolean canWalkOn(){
         return true;
     }
@@ -40,5 +69,15 @@ public class Monster extends Character implements Movable {
         if(!isAlive())
             this.Die();
 
+        movetimer.update(now);
+
+        if(movetimer.isFinished() || !movetimer.isRunnig()){
+            Direction newdir;
+            do{
+                newdir = Direction.random();
+            }while(!canMove(newdir));
+            doMove(newdir);
+            movetimer.StartTimer(now);
+        }
     }
 }
--- src/main/java/fr/ubx/poo/view/sprite/spritegameobject/SpriteMonster.java
+++ src/main/java/fr/ubx/poo/view/sprite/spritegameobject/SpriteMonster.java
@@ -1,6 +1,8 @@
 package fr.ubx.poo.view.sprite.spritegameobject;
 
 import fr.ubx.poo.model.go.GameObject;
+import fr.ubx.poo.model.go.character.Monster;
+import fr.ubx.poo.view.image.ImageFactory;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
@@ -14,9 +16,9 @@ public class SpriteMonster extends SpriteGameObject {
 
     @Override
     public void updateImage() {
-        /*
+
         Monster m = (Monster) go;
-        setImage(ImageFactory.getInstance().getMonster(m.getDirection()));*/
+        setImage(ImageFactory.getInstance().getMonster(m.getDirection()));
 
     }
 }
